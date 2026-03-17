be4dd8b752121f130653f3327b8e972c1011fba2
LAPTOP-NPHHIF4K\garat <garat.seb@gmail.com>
2026-03-17 11:55:26 +0100

    v colonnes

--- src/App.jsx
+++ src/App.jsx
@@ -3,151 +3,10 @@ import { useState } from "react";
 import "./App.css";
 
 const BLOCKS = [
-  "minecraft:air",
-  "minecraft:stone",
-  "minecraft:granite",
-  "minecraft:polished_granite",
-  "minecraft:diorite",
-  "minecraft:polished_diorite",
-  "minecraft:andesite",
-  "minecraft:polished_andesite",
+  "minecraft:sand",
   "minecraft:grass_block",
   "minecraft:dirt",
-  "minecraft:coarse_dirt",
-  "minecraft:podzol",
-  "minecraft:crimson_nylium",
-  "minecraft:warped_nylium",
-  "minecraft:crimson_roots",
-  "minecraft:warped_roots",
-  "minecraft:mycelium",
-  "minecraft:sand",
-  "minecraft:red_sand",
-  "minecraft:gravel",
-  "minecraft:cobblestone",
-  "minecraft:oak_planks",
-  "minecraft:spruce_planks",
-  "minecraft:birch_planks",
-  "minecraft:jungle_planks",
-  "minecraft:acacia_planks",
-  "minecraft:dark_oak_planks",
-  "minecraft:crimson_planks",
-  "minecraft:warped_planks",
-  "minecraft:oak_log",
-  "minecraft:spruce_log",
-  "minecraft:birch_log",
-  "minecraft:jungle_log",
-  "minecraft:acacia_log",
-  "minecraft:dark_oak_log",
-  "minecraft:crimson_stem",
-  "minecraft:warped_stem",
-  "minecraft:oak_leaves",
-  "minecraft:spruce_leaves",
-  "minecraft:birch_leaves",
-  "minecraft:jungle_leaves",
-  "minecraft:acacia_leaves",
-  "minecraft:dark_oak_leaves",
-  "minecraft:sponge",
-  "minecraft:wet_sponge",
-  "minecraft:glass",
-  "minecraft:lapis_block",
-  "minecraft:lapis_ore",
-  "minecraft:coal_ore",
-  "minecraft:iron_ore",
-  "minecraft:copper_ore",
-  "minecraft:gold_ore",
-  "minecraft:redstone_ore",
-  "minecraft:diamond_ore",
-  "minecraft:emerald_ore",
-  "minecraft:nether_gold_ore",
-  "minecraft:ancient_debris",
-  "minecraft:deepslate",
-  "minecraft:copper_block",
-  "minecraft:iron_block",
-  "minecraft:gold_block",
-  "minecraft:diamond_block",
-  "minecraft:emerald_block",
-  "minecraft:obsidian",
-  "minecraft:end_stone",
-  "minecraft:bedrock",
-  "minecraft:stone_bricks",
-  "minecraft:mossy_stone_bricks",
-  "minecraft:cracked_stone_bricks",
-  "minecraft:chiseled_stone_bricks",
-  "minecraft:sandstone",
-  "minecraft:cut_sandstone",
-  "minecraft:chiseled_sandstone",
-  "minecraft:red_sandstone",
-  "minecraft:cut_red_sandstone",
-  "minecraft:chiseled_red_sandstone",
-  "minecraft:brick_block",
-  "minecraft:clay",
-  "minecraft:terracotta",
-  "minecraft:white_terracotta",
-  "minecraft:orange_terracotta",
-  "minecraft:magenta_terracotta",
-  "minecraft:light_blue_terracotta",
-  "minecraft:yellow_terracotta",
-  "minecraft:lime_terracotta",
-  "minecraft:pink_terracotta",
-  "minecraft:gray_terracotta",
-  "minecraft:light_gray_terracotta",
-  "minecraft:cyan_terracotta",
-  "minecraft:purple_terracotta",
-  "minecraft:blue_terracotta",
-  "minecraft:brown_terracotta",
-  "minecraft:green_terracotta",
-  "minecraft:red_terracotta",
-  "minecraft:black_terracotta",
-  "minecraft:bookshelf",
-  "minecraft:crafting_table",
-  "minecraft:furnace",
-  "minecraft:blast_furnace",
-  "minecraft:smoker",
-  "minecraft:chest",
-  "minecraft:trapped_chest",
-  "minecraft:hopper",
-  "minecraft:anvil",
-  "minecraft:enchanting_table",
-  "minecraft:beacon",
-  "minecraft:jukebox",
-  "minecraft:dispenser",
-  "minecraft:dropper",
-  "minecraft:observer",
-  "minecraft:piston",
-  "minecraft:sticky_piston",
-  "minecraft:sticky_piston_head",
-  "minecraft:lever",
   "minecraft:rail",
-  "minecraft:activator_rail",
-  "minecraft:detector_rail",
-  "minecraft:powered_rail",
-  "minecraft:hay_block",
-  "minecraft:slime_block",
-  "minecraft:snow",
-  "minecraft:snow_block",
-  "minecraft:ice",
-  "minecraft:packed_ice",
-  "minecraft:blue_ice",
-  "minecraft:water",
-  "minecraft:lava",
-  "minecraft:sea_lantern",
-  "minecraft:glowstone",
-  "minecraft:shroomlight",
-  "minecraft:campfire",
-  "minecraft:soul_campfire",
-  "minecraft:lantern",
-  "minecraft:soul_lantern",
-  "minecraft:torch",
-  "minecraft:soul_torch",
-  "minecraft:cobblestone_wall",
-  "minecraft:andesite_wall",
-  "minecraft:granite_wall",
-  "minecraft:diorite_wall",
-  "minecraft:mossy_cobblestone_wall",
-  "minecraft:brick_wall",
-  "minecraft:blackstone_wall",
-  "minecraft:nether_brick_wall",
-  "minecraft:red_nether_brick_wall",
   // ajoute davantage si besoin
 ];
 
@@ -403,65 +262,71 @@ export default function App() {
   return (
     <DndContext onDragEnd={handleDragEnd}>
       <div className="app">
-        <h2>Palette Blocs</h2>
-        <input
-          type="text"
-          placeholder="Filtrer blocs..."
-          value={blockFilter}
-          onChange={(e) => setBlockFilter(e.target.value)}
-          style={{ marginBottom: 5, width: "100%" }}
-        />
-        <div className="palette">
-          {filteredBlocks.map((b) => (
-            <Draggable key={b} id={`block|${b}`} label={b} />
-          ))}
-        </div>
-
-        <h2>Palette Pokémon</h2>
-        <input
-          type="text"
-          placeholder="Filtrer Pokémon..."
-          value={pokemonFilter}
-          onChange={(e) => setPokemonFilter(e.target.value)}
-          style={{ marginBottom: 5, width: "100%" }}
-        />
-        <div className="palette">
-          {filteredPokemons.map((p) => (
-            <Draggable key={p} id={`pokemon|${p}`} label={p} />
-          ))}
-        </div>
-
-        <div style={{ margin: "10px 0" }}>
-          <button onClick={exportJSON}>Exporter JSON</button>
-
-          {/* Import JSON */}
-          <label style={{ marginLeft: 10, cursor: "pointer", color: "blue" }}>
-            Importer JSON
+        <div class="container">
+          <div class="pp">
             <input
-              type="file"
-              accept=".json"
-              onChange={importJSON}
-              style={{ display: "none" }}
+              type="text"
+              placeholder="Filtrer blocs..."
+              value={blockFilter}
+              onChange={(e) => setBlockFilter(e.target.value)}
+              style={{ marginBottom: 5, width: "100%" }}
             />
-          </label>
-        </div>
-
-        <div className="rules">
-          {rules.map((rule, rIndex) => (
-            <div key={rIndex} className="ruleRow">
-              <div className="grid">
-                {rule.pattern.map((v, i) => (
-                  <Slot key={i} id={`slot|${rIndex}|${i}`} value={v} />
-                ))}
-              </div>
-
-              <div className="pokeSlots">
-                {rule.pokemons.map((v, i) => (
-                  <Slot key={i} id={`slot|${rIndex}|${i + 9}`} value={v} />
-                ))}
-              </div>
+            <div className="palette">
+              {filteredBlocks.map((b) => (
+                <Draggable key={b} id={`block|${b}`} label={b} />
+              ))}
             </div>
-          ))}
+          </div>
+          <div class="pp">
+            <input
+              type="text"
+              placeholder="Filtrer Pokémon..."
+              value={pokemonFilter}
+              onChange={(e) => setPokemonFilter(e.target.value)}
+              style={{ marginBottom: 5, width: "100%" }}
+            />
+            <div className="palette">
+              {filteredPokemons.map((p) => (
+                <Draggable key={p} id={`pokemon|${p}`} label={p} />
+              ))}
+            </div>
+          </div>
+          <div class="pp">
+            <div className="rules">
+              {rules.map((rule, rIndex) => (
+                <div key={rIndex} className="ruleRow">
+                  <div className="grid">
+                    {rule.pattern.map((v, i) => (
+                      <Slot key={i} id={`slot|${rIndex}|${i}`} value={v} />
+                    ))}
+                  </div>
+
+                  <div className="pokeSlots">
+                    {rule.pokemons.map((v, i) => (
+                      <Slot key={i} id={`slot|${rIndex}|${i + 9}`} value={v} />
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div style={{ margin: "10px 0" }}>
+              <button onClick={exportJSON}>Exporter JSON</button>
+
+              {/* Import JSON */}
+              <label
+                style={{ marginLeft: 10, cursor: "pointer", color: "blue" }}
+              >
+                Importer JSON
+                <input
+                  type="file"
+                  accept=".json"
+                  onChange={importJSON}
+                  style={{ display: "none" }}
+                />
+              </label>
+            </div>
+          </div>
         </div>
       </div>
     </DndContext>
